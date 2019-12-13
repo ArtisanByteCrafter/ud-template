cfa2c804e9a4cbb05dbf0baaf2e292283d5ee5e2
Nathaniel Webb <natethegreat440@gmail.com>
2019-12-13 15:35:38 -0600

    remove -whatif (here we go)

--- Build/build.ps1
+++ Build/build.ps1
@@ -68,7 +68,7 @@ Switch ($true) {
                 ErrorAction = 'Stop'
             }
 
-            Publish-Module @deploySplat -WhatIf
+            Publish-Module @deploySplat
         }
         Catch {
             throw $_
